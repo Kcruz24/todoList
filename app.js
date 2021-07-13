9fc192fea14525d1083c2203f10a993c97ba04c0
Kevin Cruz <kocruz.24@gmail.com>
2021-07-12 23:35:57 -0400

    Ready for deployment on Heroku

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const User = require("./models/user");
 const { constants } = require("perf_hooks");
 
 // DB Connection
-const dbUrl = "mongodb://localhost:27017/todoList";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/todoList";
 mongoose
     .connect(dbUrl, {
         useNewUrlParser: true,
